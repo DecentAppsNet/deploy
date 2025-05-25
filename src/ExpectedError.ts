3bfbe9d15568a24a83cf4d758fd63fbd26e8dd38
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2025-05-25 11:40:42 -0700

    Delete everything as this repo will now be deployed from monorepo.

--- src/ExpectedError.ts
+++
@@ -1,9 +0,0 @@
-// For exceptions thrown by this code that correspond to expected errors like failed input validation.
-class ExpectedError extends Error { 
-  constructor(message:string) {
-    super(message);
-    this.name = "ExpectedError";
-  }
-}
-
-export default ExpectedError;
